4a09ff658b8d66b05e304f5bebcac410f0067529
bogdanbojan <71257990+bogdanbojan@users.noreply.github.com>
2021-07-31 10:19:32 +0300

    initial commit

---
+++ src/Course/Functor.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+
+module Course.Functor where
+
+import Course.Core
+import Course.ExactlyOne
+import Course.Optional
+import Course.List
+import qualified Prelude as P(fmap)
+
+-- | All instances of the `Functor` type-class must satisfy two laws. These laws
+-- are not checked by the compiler. These laws are given as:
+--
+-- * The law of identity
+--   `∀x. (id <$> x) ≅ x`
+--
+-- * The law of composition
+--   `∀f g x.(f . g <$> x) ≅ (f <$> (g <$> x))`
+class Functor k where
+  -- eff-map and lifting are pretty much the same thing.
+  -- Pronounced, eff-map.
+  (<$>) ::
+    (a -> b)
+    -> k a
+    -> k b
+
+infixl 4 <$>
+
+-- $setup
+-- >>> :set -XOverloadedStrings
+-- >>> import Course.Core
+-- >>> import qualified Prelude as P(return, (>>))
+
+-- | Maps a function on the ExactlyOne functor.
+--
+-- >>> (+1) <$> ExactlyOne 2
+-- ExactlyOne 3
+instance Functor ExactlyOne where
+  (<$>) ::
+    (a -> b)
+    -> ExactlyOne a
+    -> ExactlyOne b
+  (<$>) f (ExactlyOne a) = ExactlyOne (f a)
+
+-- | Maps a function on the List functor.
+--
+-- >>> (+1) <$> Nil
+-- []
+--
+-- >>> (+1) <$> (1 :. 2 :. 3 :. Nil)
+-- [2,3,4]
+instance Functor List where
+  (<$>) ::
+    (a -> b)
+    -> List a
+    -> List b
+  (<$>) =
+    error "todo: Course.Functor (<$>)#instance List"
+
+-- | Maps a function on the Optional functor.
+--
+-- >>> (+1) <$> Empty
+-- Empty
+--
+-- >>> (+1) <$> Full 2
+-- Full 3
+instance Functor Optional where
+  (<$>) ::
+    (a -> b)
+    -> Optional a
+    -> Optional b
+  (<$>) = mapOptional
+
+-- | Maps a function on the reader ((->) t) functor.
+--
+-- >>> ((+1) <$> (*2)) 8
+-- 17
+instance Functor ((->) t) where
+  (<$>) ::
+    (a -> b)
+    -> ((->) t a)
+    -> ((->) t b)
+  (<$>) f g  =  \t -> f (g t) -- can be reduced :  f . g => (.) f g => (.)
+      
+
+-- | Anonymous map. Maps a constant value on a functor.
+--
+-- >>> 7 <$ (1 :. 2 :. 3 :. Nil)
+-- [7,7,7]
+--
+-- prop> \x a b c -> x <$ (a :. b :. c :. Nil) == (x :. x :. x :. Nil)
+--
+-- prop> \x q -> x <$ Full q == Full x
+(<$) ::
+  Functor k =>
+  a
+  -> k b
+  -> k a
+(<$) a kb = const a <$> kb 
+-- | Anonymous map producing unit value.
+--
+-- >>> void (1 :. 2 :. 3 :. Nil)
+-- [(),(),()]
+--
+-- >>> void (Full 7)
+-- Full ()
+--
+-- >>> void Empty
+-- Empty
+--
+-- >>> void (+10) 5
+-- ()
+void ::
+  Functor k =>
+  k a
+  -> k ()
+void ka = const () <$> ka
+
+-----------------------
+-- SUPPORT LIBRARIES --
+-----------------------
+
+-- | Maps a function on an IO program.
+--
+-- >>> reverse <$> (putStr "hi" P.>> P.return ("abc" :: List Char))
+-- hi"cba"
+instance Functor IO where
+  (<$>) =
+    P.fmap
